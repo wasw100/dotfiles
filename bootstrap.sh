95c5e7704de09b9d305b2c8a5dbd080ce2f09dcb
wasw100 <wasw100@mail.com>
2014-12-16 17:21:19 +0800

    mac和linux下sed参数不一样

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,20 +5,24 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-        rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-                --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-        # source ~/.bash_profile;
-        # 更改pydiction-dict目录的位置
-        sed -i'' "s|\$HOME|$HOME|" $HOME/.vimrc
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+    # source ~/.bash_profile;
+    # 更改pydiction-dict目录的位置, mac下的sed和linux不一样,需要判断下
+    if [[ `uname` == 'Darwin' ]]; then
+        sed -i "" "s|\$HOME|$HOME|" $HOME/.vimrc
+    else
+        sed -i "s|\$HOME|$HOME|" $HOME/.vimrc
+    fi
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-        doIt;
+    doIt;
 else
-        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-        echo "";
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-                doIt;
-        fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
