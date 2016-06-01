bc42cd2d3c061786142cb72d7407cff90f116e0a
wasw100 <wasw100@gmail.com>
2016-06-01 10:52:26 +0800

    颜色样式, pathogen, .vimrc做了新的重构

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 git submodule init
 git submodule update
+# http://stackoverflow.com/questions/5828324/update-git-submodule-to-latest-commit-on-origin
+git submodule foreach git pull origin master
 
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+    mkdir -p ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
     # source ~/.bash_profile;
 }
 
